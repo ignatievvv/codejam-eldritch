a69c432d4a5187476c29ec95a1c7d178573bd2fb
ignatievvv <ramorusbadrobot@gmail.com>
2022-08-27 12:06:09 +0300

    feat: add ancients, change layout

--- data/ancients.js
+++ data/ancients.js
@@ -1,5 +1,233 @@
 const ancientsData = [
   {
+    id: "abhoth",
+    name: "abhoth",
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 3,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: "antediluvium",
+    name: "antediluvium",
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: "atlachnacha",
+    name: "atlachnacha",
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 3,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: "elderthings",
+    name: "elderthings",
+    firstStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 3,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 4,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: "hastur",
+    name: "hastur",
+    firstStage: {
+      greenCards: 0,
+      blueCards: 2,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 5,
+    },
+  },
+  {
+    id: "hypnos",
+    name: "hypnos",
+    firstStage: {
+      greenCards: 0,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: "ithaqua",
+    name: "ithaqua",
+    firstStage: {
+      greenCards: 0,
+      blueCards: 2,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 4,
+      blueCards: 0,
+      brownCards: 2,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: "nephrenka",
+    name: "nephrenka",
+    firstStage: {
+      greenCards: 0,
+      blueCards: 2,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 1,
+      blueCards: 0,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: "nyarlathotep",
+    name: "nyarlathotep",
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: "shuddemell",
+    name: "shuddemell",
+    firstStage: {
+      greenCards: 0,
+      blueCards: 2,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 4,
+      blueCards: 0,
+      brownCards: 2,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: "syzygy",
+    name: "syzygy",
+    firstStage: {
+      greenCards: 0,
+      blueCards: 2,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 5,
+    },
+  },
+  {
+    id: "yig",
+    name: "yig",
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
     id: "azathoth",
     name: "azathoth",
     firstStage: {
